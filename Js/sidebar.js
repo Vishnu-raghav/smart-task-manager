ffc127cc2e8a8a0266960f713e26326622658bdf
Vishnu <vishnur1225@gmail.com>
2026-01-18 22:18:16 +0530

    Refactor CSS styles for sidebar and header; enhance task card layout in HTML

--- Js/sidebar.js
+++ Js/sidebar.js
@@ -1,19 +1,21 @@
-const body = document.querySelector("body")
-     sidebar = body.querySelector(".sidebar")
-     toggle = body.querySelector(".toggle")
+const body = document.body;
+const sidebar = document.querySelector(".sidebar");
+const toggle = document.querySelector(".toggle");
 
-
-
-function handleResize(){
-     if(window.innerWidth <= 768){
-          sidebar.classList.add("close")
-     }else{
-          sidebar.classList.remove("close")
-     }
+function handleResize() {
+  if (window.innerWidth <= 768) {
+    sidebar.classList.add("close");
+    body.classList.add("sidebar-close");
+  } else {
+    sidebar.classList.remove("close");
+    body.classList.remove("sidebar-close");
+  }
 }
 
 window.addEventListener("resize", handleResize);
-handleResize()
+handleResize();
+
 toggle.addEventListener("click", () => {
-     sidebar.classList.toggle("close")
-})
+  sidebar.classList.toggle("close");
+  body.classList.toggle("sidebar-close"); // ⭐ important
+});
